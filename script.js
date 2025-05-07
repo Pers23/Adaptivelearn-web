233793ba65259cd0c605431c26d1b4f1be8a54b6
Pers23 <persmaumane@gmail.com>
2025-05-07 22:38:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 document.querySelectorAll('.subject-card').forEach(card => {
   card.addEventListener('click', () => {
-    alert('You selected ' + card.textContent);
+    const subject = card.textContent.toLowerCase();
+    if (subject.includes("math")) window.location.href = "math.html";
+    else if (subject.includes("science")) window.location.href = "science.html";
+    else if (subject.includes("english")) window.location.href = "english.html";
   });
 });
